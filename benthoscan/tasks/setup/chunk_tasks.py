4b6b682eba9b71fe62b1e27606f51e7f2c6bebae
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2024-04-10 14:33:02 +0200

    Implement functions for add camera assemblies to a chunk, including image files and spatial references for the cameras.

--- benthoscan/tasks/setup/chunk_tasks.py
+++ benthoscan/tasks/setup/chunk_tasks.py
@@ -1,9 +1,9 @@
-""" Functions for """
+"""Functions for chunk configuration - adding images, cameras, and reference to chunks."""
 
 from collections import OrderedDict
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Dict, List, Tuple, TypeAlias
+from typing import Callable, Dict, List, TypeAlias
 
 import Metashape
 
@@ -11,18 +11,16 @@ from loguru import logger
 from result import Ok, Err, Result
 
 from benthoscan.containers import FileRegistry
-from benthoscan.datatypes import CameraAssembly
+from benthoscan.datatypes import Camera, CameraAssembly
 from benthoscan.project import Chunk
 
-GroupKey: TypeAlias = str
-Index: TypeAlias = int
-
+ProgressCallback: TypeAlias = Callable[[float], None]
 
 def add_assembly_images(
     chunk: Chunk,
     assemblies: List[CameraAssembly],
     registry: FileRegistry,
-    progress_fun: Callable[[float], None] = lambda percent: None,
+    progress_fun: ProgressCallback = lambda percent: None,
 ) -> Result[int, str]:
     """Adds multiplane images from camera assemblies to the chunk. The image
     paths are retrieved from the file registry before being add into Metashape."""
@@ -43,6 +41,11 @@ def add_assembly_images(
         filenames.extend([registry[camera.labels.image] for camera in assembly.cameras])
         filegroups.append(assembly.count)
 
+    filenames = [str(filename) for filename in filenames]
+
+    logger.info(f"Filenames:  {len(filenames)}")
+    logger.info(f"Filegroups: {len(filegroups)}")
+
     try:
         chunk.addPhotos(
             filenames=filenames,
@@ -53,57 +56,101 @@ def add_assembly_images(
     except RuntimeError as error:
         return Err(str(error))
 
-    return Ok(len(filegroups))
+    success_count, input_count = len(chunk.cameras), len(filenames)
+    if input_count != success_count:
+        # TODO: Retrieve the camera labels for the files that have not been added
+        return Err(f"failed to add all cameras to chunk: {input_count} input, {success_count} added")
+
+    stats = {
+        "filename_count": len(filenames),
+        "filegroup_count": len(filegroups),
+        "added_cameras": len(chunk.cameras),
+        "given_cameras": len(filenames),
+    }
+
+    return Ok(stats)
+
+
+def add_camera_reference(
+    camera: Metashape.Camera,
+    reference: Camera.Reference,
+) -> bool:
+    """Updates the reference of a Metashape camera with the values from a camera
+    reference."""
+    # Update state to make a potentially old reference invalid
+    camera.reference.enabled = False
+    camera.reference.location_enabled = False
+    camera.reference.rotation_enabled = False
+
+    if reference.has_position:
+        camera.reference.enabled = True
+        camera.reference.location_enabled = True
+        camera.reference.location = Metashape.Vector((
+            reference.position.x,
+            reference.position.y,
+            reference.position.z,
+        ))
+
+    if reference.has_position_accuracy:
+        camera.reference.location_accuracy = Metashape.Vector((
+            reference.position_accuracy.x,
+            reference.position_accuracy.y,
+            reference.position_accuracy.z,
+        ))
+
+    if reference.has_orientation:
+        camera.reference.rotation_enabled = True
+        camera.reference.rotation = Metashape.Vector((
+            reference.orientation.x,
+            reference.orientation.y,
+            reference.orientation.z,
+        ))
+    
+    if reference.has_orientation_accuracy:
+        camera.reference.rotation_accuracy = Metashape.Vector((
+            reference.orientation_accuracy.x,
+            reference.orientation_accuracy.y,
+            reference.orientation_accuracy.z,
+        ))
+
+    return True
 
 
 def add_assembly_references(
     chunk: Chunk,
     assemblies: List[CameraAssembly],
-    progress_fun: Callable[[float], None] = lambda percent: None,
-) -> Result[int, str]:
-    """TODO"""
+    progress_fun: ProgressCallback = lambda percent: None,
+) -> Result[Dict, str]:
+    """Adds references to the cameras in a collection of camera assemblies."""
 
-    for camera in chunk.cameras:
-        logger.info(camera)
+    stats = {
+        "successes": list(),
+        "failures": list(),
+    }
 
-    raise NotImplementedError
+    # Set up a lookup table for the chunk cameras
+    chunk_cameras = {camera.label: camera for camera in chunk.cameras}
 
+    for assembly in assemblies:
+        for camera in assembly.cameras:
+            key = camera.labels.camera
 
-def add_camera_references(
-    chunk: Chunk,
-    camera_references,
-) -> Result[bool, str]:
-    """Add reference from memory."""
+            if not camera.has_reference:
+                continue
 
-    logger.info(f"Reference: {reference.fields}")
-    logger.info(f"Config:    {config}")
+            if not key in chunk_cameras:
+                logger.error(f"unable to find camera in chunk: {key}")
+                continue
 
-    input("Press a key...")
+            chunk_camera = chunk_cameras[key]
 
-    # TODO: Get reference keys
-    labels = list(reference.get_field(config.label_field).values())
-    labels = list(reference.get_field(config.label_field).values())
+            # Add reference to chunk camera
+            success = add_camera_reference(chunk_camera, camera.reference)
 
-    # labels = list(reference.get_field("stereo_left_label").values())
+            if not success:
+                logger.warning(f"failed to add reference to camera: {key}")
+                stats["failures"].append(key)
+            else:
+                stats["successes"].append(key)
 
-    reference_data = reference.items()
-
-    for item in reference_data:
-        logger.info(f"Index: {item}")
-
-    input("Press a key...")
-
-    # Camera lookup
-    camera_lookup = dict([(camera.label, camera) for camera in chunk.cameras])
-
-    for label in labels:
-        logger.info(f"Label:  {label}")
-
-    for camera in chunk.cameras:
-        logger.info(f"Camera: {camera.label}")
-        if camera.label in labels:
-            logger.info("Left")
-        else:
-            logger.info("Unknown")
-
-    return Ok(True)
+    return Ok(stats)
