77e561fd2e12015c6e464abcd5dff2c9dbbc2d76
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2024-08-18 10:33:44 +0200

    Add msgspec and file reader and writers.

--- src/benthoscan/io/__init__.py
+++ src/benthoscan/io/__init__.py
@@ -1,4 +1,34 @@
-"""Package with various input/output functions, including reading and writing to TOML, YAML, and JSON files."""
+"""Package with various input/output functions, including reading and writing to JSON,
+TOML, YAML, and MSGPACK files."""
 
-from .file_readers import read_dict_from_file, read_csv, read_json, read_toml, read_yaml
-from .file_writers import write_dict_to_file, write_json, write_toml, write_yaml
+from .file_readers import (
+    read_dict_from_file,
+    read_csv,
+    read_json,
+    read_toml,
+    read_yaml,
+    read_msgpack,
+)
+
+from .file_writers import (
+    write_dict_to_file,
+    write_json,
+    write_toml,
+    write_yaml,
+    write_msgpack,
+)
+
+
+__all__ = [
+    "read_dict_from_file",
+    "read_csv",
+    "read_json",
+    "read_toml",
+    "read_yaml",
+    "read_msgpack",
+    "write_dict_to_file",
+    "write_json",
+    "write_toml",
+    "write_yaml",
+    "write_msgpack",
+]
