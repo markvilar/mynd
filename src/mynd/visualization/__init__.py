8859700e0ae59a93ae28178e455ef72796ba5c07
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2024-10-22 19:40:42 +0200

    Refactor visualization package.

--- src/mynd/visualization/__init__.py
+++ src/mynd/visualization/__init__.py
@@ -1,39 +1,44 @@
 """Package with visualization functionality including plotting, and 2D/3D rendering."""
 
-from .data_drawers import (
+from .geometry_renderers import (
     visualize_registration,
     visualize_registration_batch,
 )
 
-from .data_plotters import (
+from .geometry_plotting import (
+    create_subplots,
     trace_registration_result,
 )
 
-from .image_visualizers import (
+from .image_renderers import (
     WindowHandle,
     TrackbarData,
-    create_image_visualizer,
+    create_window,
     colorize_values,
     render_image,
     destroy_window,
     destroy_all_windows,
     wait_key_input,
+    StereoWindows,
+    create_stereo_windows,
+    render_stereo_geometry,
 )
-
-from .plot_factories import create_subplots
 
 
 __all__ = [
     "visualize_registration",
     "visualize_registration_batch",
-    "trace_registration_result",
     "WindowHandle",
     "TrackbarData",
-    "create_image_visualizer",
+    "create_window",
     "colorize_values",
     "render_image",
     "destroy_window",
     "destroy_all_windows",
     "wait_key_input",
+    "StereoWindows",
+    "create_stereo_windows",
+    "render_stereo_geometry",
     "create_subplots",
+    "trace_registration_result",
 ]
